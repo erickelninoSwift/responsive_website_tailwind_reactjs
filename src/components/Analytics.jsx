da9d798063a401d3f72f1d9dd0d343c23acdae8c
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-02 00:26:39 +0200

    update

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -4,8 +4,8 @@ export const Analytics = () => {
   return (
     <div className="w-full bg-white py-16 px-4">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <img src={Laptop} alt="Laptop" />
-        <div>
+        <img className="w-[500px] mx-auto my-4" src={Laptop} alt="Laptop" />
+        <div className="flex flex-col justify-center gap-2">
           <p>DATA ANALYTICS DASHBOARD</p>
           <h1>Manage Data Analytics Centrally</h1>
           <p>
