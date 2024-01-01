37314a3ded2c8d3b64e8348f16734ba53e4614dc
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-01 21:04:19 +0200

    update

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-
+import Laptop from "../assets/laptop.jpg";
 export const Analytics = () => {
-  return <div className="text-white">Analytics</div>;
+  return (
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <img src={Laptop} alt="Laptop" />
+        <div>
+          <p>DATA ANALYTICS DASHBOARD</p>
+          <h1>Manage Data Analytics Centrally</h1>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            molestiae delectus culpa hic assumenda, voluptate reprehenderit
+            dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
+            eveniet ex deserunt fuga?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
