7667815858d06cb09494fbf640f7781de7663be1
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-02 15:22:52 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,22 @@ import {
 export const Footer = () => {
   return (
     <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-500">
-      Footer
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <p className="py-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          molestiae delectus culpa hic assumenda, voluptate reprehenderit dolore
+          autem cum ullam sed odit perspiciatis. Doloribus quos velit, eveniet
+          ex deserunt fuga?
+        </p>
+        <p className="flex flex-row gap-1">
+          <FaDribbbleSquare size={50} />
+          <FaFacebookSquare size={50} />
+          <FaGithubSquare size={50} />
+          <FaInstagram size={50} />
+          <FaTwitterSquare size={50} />
+        </p>
+      </div>
     </div>
   );
 };
