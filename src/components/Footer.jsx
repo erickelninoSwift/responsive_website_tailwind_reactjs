4fccc4cb8dbfc304405199bd197318732f91be9c
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-02 16:07:19 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,18 +13,56 @@ export const Footer = () => {
       <div>
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
         <p className="py-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          molestiae delectus culpa hic assumenda, voluptate reprehenderit dolore
-          autem cum ullam sed odit perspiciatis. Doloribus quos velit, eveniet
-          ex deserunt fuga?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. molestiae
+          delectus culpa hic assumenda, voluptate reprehenderit dolore autem cum
+          ullam sed odit perspiciatis. Doloribus quos
         </p>
-        <p className="flex flex-row gap-1">
+        <div className="flex md:w-[75%] my-6 justify-between">
           <FaDribbbleSquare size={50} />
           <FaFacebookSquare size={50} />
           <FaGithubSquare size={50} />
           <FaInstagram size={50} />
           <FaTwitterSquare size={50} />
-        </p>
+        </div>
+      </div>
+
+      <div className="lg:col-span-2 flex justify-between">
+        <div>
+          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <ul>
+            <li className="py-2 text-sm">Analytics</li>
+            <li className="py-2 text-sm">Marketing</li>
+            <li className="py-2 text-sm">Commerce</li>
+            <li className="py-2 text-sm">Insights</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Support</h6>
+          <ul>
+            <li className="py-2 text-sm">Pricing</li>
+            <li className="py-2 text-sm">Documentation</li>
+            <li className="py-2 text-sm">Guides</li>
+            <li className="py-2 text-sm">API Status</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Company</h6>
+          <ul>
+            <li className="py-2 text-sm">About</li>
+            <li className="py-2 text-sm">Blog</li>
+            <li className="py-2 text-sm">Jobs</li>
+            <li className="py-2 text-sm">Press</li>
+            <li className="py-2 text-sm">Careers</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm">Claim</li>
+            <li className="py-2 text-sm">Policy</li>
+            <li className="py-2 text-sm">Terms</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
