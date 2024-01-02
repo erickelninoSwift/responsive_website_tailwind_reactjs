ae827a6326c2e2b87f4f6b28ab9c19fbe7afc6e3
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-02 15:06:17 +0200

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
+export const Footer = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-500">
+      Footer
+    </div>
+  );
+};
