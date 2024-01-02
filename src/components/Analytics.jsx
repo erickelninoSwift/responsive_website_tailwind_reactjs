c1c31983c8ca24733ba7ccd6842bd1a994ca22f2
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-02 14:09:39 +0200

    update

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -6,8 +6,12 @@ export const Analytics = () => {
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
         <img className="w-[500px] mx-auto my-4" src={Laptop} alt="Laptop" />
         <div className="flex flex-col justify-center gap-2">
-          <p>DATA ANALYTICS DASHBOARD</p>
-          <h1>Manage Data Analytics Centrally</h1>
+          <p className="text-[#00df9a] font-bold text-[25px]">
+            DATA ANALYTICS DASHBOARD
+          </p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Manage Data Analytics Centrally
+          </h1>
           <p>
             {" "}
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
@@ -15,6 +19,9 @@ export const Analytics = () => {
             dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
             eveniet ex deserunt fuga?
           </p>
+          <button className="bg-black text-[#00df9a] w-[200px] text-center rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Newsletter = () => {
+export const NewsLetter = () => {
   return (
-    <div className="w-full py-16 text-white px-4">
+    <div className="text-white w-full py-16  px-4">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
-        <div className="lg:col-span-2 my-4">
-          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+        <div className="lg:col-span-2">
+          <h1 className="md:text-4xl sm:text-3xl  text-2xl font-bold py-2">
+            {" "}
             Want tips & tricks to optimize your flow?
           </h1>
           <p>Sign up to our newsletter and stay up to date.</p>
@@ -13,12 +14,12 @@ const Newsletter = () => {
         <div className="my-4">
           <div className="flex flex-col sm:flex-row items-center justify-between w-full">
             <input
-              className="p-3 flex w-full rounded-md text-black"
               type="email"
               placeholder="Enter Email"
+              className="p-3 flex rounded-md w-full text-black"
             />
-            <button className="bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3">
-              Notify Me
+            <button className="w-[200px] rounded-md font-medium bg-[#00df9a] text-black ml-4 my-6 mx-auto px-6 py-3">
+              Notify me
             </button>
           </div>
           <p>
@@ -30,5 +31,3 @@ const Newsletter = () => {
     </div>
   );
 };
-
-export default Newsletter;
